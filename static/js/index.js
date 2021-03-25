de11c54f9cbf1b3e2bb7d5b81b442e0346d329f9
marco7524 <80277084+marco7524@users.noreply.github.com>
2021-03-24 21:11:09 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -70,5 +70,6 @@ function LED1_Off(){
   // called when a message arrives
   function onMessageArrived(message) {
     console.log("onMessageArrived:"+message.payloadString);
+	  document.getElementById("sensor").innerHTML=message.payloadString);
   }
   
