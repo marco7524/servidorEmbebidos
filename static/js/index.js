3ec9675acb795c14e943832dab7944ed54fa8d4a
marco7524 <80277084+marco7524@users.noreply.github.com>
2021-03-24 22:36:06 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -86,13 +86,13 @@ function cambio()
     console.log("onMessageArrived:"+message.payloadString);
 	  //comando para poner el sensor desde esp32
 	  document.getElementById("sensor").innerHTML=message.payloadString;
-	  if message.payloadString==='Encendido'){
+	  if(message.payloadString==='Encendido'){
 		 document.getElementById("imagen").src="http://www.clker.com/cliparts/M/h/R/9/8/H/red-led-on-md.png";
 	  } else if message.payloadString==='Apagado'){
 		 document.getElementById("imagen").src="http://www.clker.com/cliparts/M/h/R/9/8/H/red-led-off-md.png";
  		
 	  }
-	  if message.payloadString==='Encendido'){
+	  if(message.payloadString==='Encendido'){
 	  	document.getElementById("btn").innerHTML="Apagar";
 	  } else if message.payloadString==='Apagado'){
 		document.getElementById("btn").innerHTML="Apagar";
